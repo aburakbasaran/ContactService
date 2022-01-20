8789cd44958432dbd4750fb956c810d048e4b7dd
DESKTOP-CCGGG8H\aburak <burak.basaran@odeontechnology.com>
2022-01-20 16:54:39 +0300

    Swagger Added.

---
+++ Source/Core/ContractService.Infrastructure/Extension/SwaggerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using ContactService.Infrastructure.Constant;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag;
+using System;
+
+namespace ContactService.Infrastructure.Extension
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerDocument(configure =>
+            {
+                configure.PostProcess = document =>
+                {
+                    document.Info.Version = SwaggerConstants.InfoVersion1;
+                    document.Info.Title = SwaggerConstants.InfoTitle;
+                    document.Info.Description = SwaggerConstants.InfoDescription;
+                    document.Info.Contact = new()
+                    {
+                        Name = SwaggerConstants.InfoContactName,
+                        Url = SwaggerConstants.Description
+                    };
+                };
+
+                configure.AddSecurity(SwaggerConstants.Bearer, Array.Empty<string>(), new()
+                {
+                    Description = SwaggerConstants.Description,
+                    Name = SwaggerConstants.Authorization,
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Scheme = SwaggerConstants.Bearer,
+                    BearerFormat = SwaggerConstants.Jwt,
+                });
+            });
+            return services;
+        }
+    }
+}
--- Source/Service/ContactService.API/Startup.cs
+++ Source/Service/ContactService.API/Startup.cs
@@ -1,8 +1,13 @@
+using ContactService.Infrastructure.Extension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +17,19 @@ namespace ContactService.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllers();
+            services.AddSwaggerConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +40,19 @@ namespace ContactService.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseOpenApi();
+
+            app.UseSwaggerUi3();
+
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
