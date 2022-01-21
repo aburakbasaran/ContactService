aee908353c3d8f86668f09cf44414ea9de260b91
DESKTOP-CCGGG8H\aburak <burak.basaran@odeontechnology.com>
2022-01-21 22:41:17 +0300

    Igranered file update.

---
+++ Source/Module/Contact/ContactService.ContactModule.Engine/BootStrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using ContactService.ContactModule.Data.Data;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactService.ContactModule.Engine
+{
+    public static class BootStrapper
+    {
+        public static IServiceCollection AddContactModuleEngine(this IServiceCollection services, IMvcBuilder mvcBuilder, IConfiguration configuration)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+            // DB
+            services.AddContactDbContext(configuration);
+
+            // MediatR
+            services.AddMediatR(executingAssembly);
+
+            // Validators
+            services.AddValidatorsFromAssembly(executingAssembly);
+
+            // Add MVc Parts
+            mvcBuilder.AddApplicationPart(executingAssembly);
+            return services;
+        }
+    }
+}
